7e33e72eec86a2287731291c28b4acf11e1a7acb
[Gabriela] <gabriela.sikora@code.berlin>
2019-09-25 18:32:18 +0200

    Set up choose login registration activity

---
+++ app/src/main/java/com/example/capstonecode/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.capstonecode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+    }
+}
