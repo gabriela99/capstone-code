5bc22f280e3d6887da24697a3fa933569c3ce971
[Gabriela] <gabriela.sikora@code.berlin>
2019-09-30 14:28:47 +0200

    Log out button created on profile page

--- app/src/main/java/com/example/capstonecode/ProfileActivity.java
+++ app/src/main/java/com/example/capstonecode/ProfileActivity.java
@@ -2,13 +2,42 @@ package com.example.capstonecode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView userEmail;
+    Button userLogout;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        userEmail = findViewById(R.id.tvUserEmail);
+        userLogout = findViewById(R.id.btnLogout);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        userEmail.setText(firebaseUser.getEmail());
+
+        userLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
     }
 }
