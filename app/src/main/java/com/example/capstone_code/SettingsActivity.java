ebe80e42f1e9111d6fd2dc1667ed3383a1f47388
[Gabriela] <gabriela.sikora@code.berlin>
2019-10-07 10:45:12 +0200

    Add event listener to go back to profile page from settings page

--- app/src/main/java/com/example/capstone_code/SettingsActivity.java
+++ app/src/main/java/com/example/capstone_code/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.capstone_code;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -9,14 +11,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private ImageView ivProfileImage;
-    private EditText etUserName, etEmail, etRole, etSkills;
-    private Button btnConfirmSettings, btnBackToProfile;
+    private ImageView mProfileImage;
+    private EditText mUserName, mEmail, mRole, mSkills;
+    private Button confirmSettings, backToProfile;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
+        mUserName = (EditText) findViewById(R.id.etUserName);
+        mEmail = (EditText) findViewById(R.id.etEmail);
+        mRole = (EditText) findViewById(R.id.etRole);
+        mSkills = (EditText) findViewById(R.id.etSkills);
+        confirmSettings = (Button) findViewById(R.id.btnConfirmSettings);
+        backToProfile = (Button) findViewById(R.id.btnBackToProfile);
+
+
+        backToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
