03ce148b01c67e9e266f92f3bc79e4cee50c4707
[Gabriela] <gabriela.sikora@code.berlin>
2019-10-08 10:39:18 +0200

    Add method for getting user info

--- app/src/main/java/com/example/capstone_code/SettingsActivity.java
+++ app/src/main/java/com/example/capstone_code/SettingsActivity.java
@@ -7,11 +7,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -70,9 +77,45 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     private void getUserInfo() {
-    }
+        mCustomerDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("name")!=null){
+                        String name = map.get("name").toString();
+                        mUserName.setText(name);
+                    }
+                    if(map.get("role")!=null){
+                        String role = map.get("role").toString();
+                        mRole.setText(role);
+                    }
+                    if(map.get("skills")!=null){
+                        String skills = map.get("skills").toString();
+                        mSkills.setText(skills);
+                    }
+                    Glide.with(mProfileImage).clear(mProfileImage);
+                    if(map.get("profileImageUrl")!=null){
+                        String profileImageUrl = map.get("profileImageUrl").toString();
+                        switch(profileImageUrl){
+                            case "default":
+                                Glide.with(getApplication()).load(R.mipmap.ic_launcher).into(mProfileImage);
+                                break;
+                            default:
+                                Glide.with(getApplication()).load(profileImageUrl).into(mProfileImage);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     private void saveUserInformation() {
-        
+
     }
 }
