8581ce2376664759f680b7a1d3715c0439bcf29a
[Gabriela] <gabriela.sikora@code.berlin>
2019-11-05 15:58:01 +0100

    Remove reminants of the second attempt at user list

--- app/src/main/java/com/example/capstone_code/viewmodel/LoginActivity.java
+++ app/src/main/java/com/example/capstone_code/viewmodel/LoginActivity.java
@@ -11,7 +11,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.capstone_code.MainActivity;
 import com.example.capstone_code.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
