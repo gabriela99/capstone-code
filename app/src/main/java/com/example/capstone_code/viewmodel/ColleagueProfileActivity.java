2927fcb9f122bf7902011875caf743509cfe14ac
[Gabriela] <gabriela.sikora@code.berlin>
2019-11-18 13:53:59 +0100

    Add javadocs and comments to colleague profile activity

--- app/src/main/java/com/example/capstone_code/viewmodel/ColleagueProfileActivity.java
+++ app/src/main/java/com/example/capstone_code/viewmodel/ColleagueProfileActivity.java
@@ -9,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.capstone_code.R;
 
+/**
+ * Take information from adapter and pass into xml fields for colleagues
+ * in order to see relevant information when colleague profile is viewed
+ */
 public class ColleagueProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "ColleagueProfileActivit";
@@ -27,7 +31,7 @@ public class ColleagueProfileActivity extends AppCompatActivity {
     }
 
     /**
-     *
+     * Gets information from adapter to pass into XML fields
      */
     private void getIncomingIntent() {
         Log.d(TAG, "getIncomingIntent: checking for incoming intents.");
@@ -36,31 +40,35 @@ public class ColleagueProfileActivity extends AppCompatActivity {
         if(getIntent().hasExtra("colleague_name")
                 && getIntent().hasExtra("colleague_role")
                 && getIntent().hasExtra("colleague_skills")){
+
             Log.d(TAG, "getIncomingIntent: found intent extras.");
 
+            // Get field information for colleague from adapter (putExtra)
 //            String imageUrl =  getIntent().getStringExtra("image_url");
             String colleagueName = getIntent().getStringExtra("colleague_name");
             String colleagueRole = getIntent().getStringExtra("colleague_role");
             String colleagueSkills = getIntent().getStringExtra("colleague_skills");
 
-
+            // Pass in information from adapter into corresponding XML fields
             setInfo(colleagueName, colleagueRole, colleagueSkills);
         }
     }
 
     /**
-     *
-     * @param colleagueName
-     * @param colleagueRole
-     * @param colleagueSkills
+     * Fills the colleague fields with the corresponding passed in information
+     * @param colleagueName name to be added to the view
+     * @param colleagueRole role to be added to the view
+     * @param colleagueSkills skills to be added to the view
      */
     private void setInfo(String colleagueName, String colleagueRole, String colleagueSkills) {
         Log.d(TAG, "setImage: setting the image and name to widgets.");
 
+        // Find XML fields
         TextView name = findViewById(R.id.tvNameColleague);
         TextView role = findViewById(R.id.tvRoleColleague);
         TextView skills = findViewById(R.id.tvSkillsColleague);
 
+        // Fill XML fields with provided parameters
         name.setText(colleagueName);
         role.setText(colleagueRole);
         skills.setText(colleagueSkills);
