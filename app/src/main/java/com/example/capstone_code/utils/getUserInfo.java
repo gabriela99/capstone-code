070b990a046470d5b23d3bdd2e21371122247f7b
Unknown <gabriela.sikora@code.berlin>
2019-11-19 13:09:04 +0100

    Move getUserInfo out of settings and into own class

---
+++ app/src/main/java/com/example/capstone_code/utils/getUserInfo.java
@@ -0,0 +1,71 @@
+package com.example.capstone_code.utils;
+
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
+public class getUserInfo {
+    private ImageView mProfileImage;
+    private EditText mUserName, mRole, mSkills;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mCustomerDatabase;
+    private String userId, name, role, skills, profileImageUrl;
+
+    public void getUserInfoFromDatabase(final EditText mUserName, final EditText mRole, final EditText mSkills) {
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+
+        mCustomerDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            /**
+             *
+             * @param dataSnapshot
+             */
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+                    Object name = map.get("name");
+                    Object role = map.get("role");
+                    Object skills = map.get("skills");
+
+                    String cleanName = cleanMapField(name);
+                    String cleanRole = cleanMapField(role);
+                    String cleanSkills = cleanMapField(skills);
+
+                    mUserName.setText(cleanName);
+                    mRole.setText(cleanRole);
+                    mSkills.setText(cleanSkills);
+                }
+            }
+
+            /**
+             *
+             * @param databaseError
+             */
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                return;
+            }
+        });
+
+    }
+
+    private String cleanMapField(Object field) {
+        if (field != null) {
+            return field.toString().trim();
+        }
+        return null;
+    }
+}
--- app/src/main/java/com/example/capstone_code/viewmodel/SettingsActivity.java
+++ app/src/main/java/com/example/capstone_code/viewmodel/SettingsActivity.java
@@ -18,16 +18,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
 import com.example.capstone_code.R;
+import com.example.capstone_code.utils.getUserInfo;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -49,7 +46,6 @@ public class SettingsActivity extends AppCompatActivity {
     private String[] roleOptions = { "Interaction Designer", "Software Engineer", "Product Manager" };
     private String[] skillOptions = { "Design thinking", "Agile", "Interpersonal skills", "Java" };
 
-
     /**
      *
      * @param savedInstanceState
@@ -62,16 +58,18 @@ public class SettingsActivity extends AppCompatActivity {
         findLayoutFields();
         instantiateFirebase();
         pickProfileImage();
-        getUserInfo();
+//        getUserInfo();
 
         autoCompleteText(roleOptions, mRole);
         autoCompleteText(skillOptions, mSkills);
-    }
 
-    private void instantiateFirebase() {
-        mAuth = FirebaseAuth.getInstance();
-        userId = mAuth.getCurrentUser().getUid();
-        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+//        autoCompleteText autoCompleteText = new autoCompleteText();
+//        autoCompleteText.autoCompleteText(roleOptions, mRole);
+//        autoCompleteText.autoCompleteText(skillOptions, mSkills);
+
+        getUserInfo getUserInfo = new getUserInfo();
+        getUserInfo.getUserInfoFromDatabase(mUserName, mRole, mSkills);
+
     }
 
     private void findLayoutFields() {
@@ -79,6 +77,12 @@ public class SettingsActivity extends AppCompatActivity {
         mUserName = findViewById(R.id.etUserName);
         mRole = findViewById(R.id.etRole);
         mSkills = findViewById(R.id.etSkills);
+    }
+
+    private void instantiateFirebase() {
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
     }
 
     private void pickProfileImage() {
@@ -92,58 +96,58 @@ public class SettingsActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     *
-     */
-    private void getUserInfo() {
-        mCustomerDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-            /**
-             *
-             * @param dataSnapshot
-             */
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
-                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
-
-                    // Redundant, make method ******
-                    if (map.get("name") != null) {
-                        name = map.get("name").toString().trim();
-                        mUserName.setText(name);
-                    }
-                    if (map.get("role") != null) {
-                        String role = map.get("role").toString().trim();
-                        mRole.setText(role);
-                    }
-                    if (map.get("skills") != null) {
-                        String skills = map.get("skills").toString().trim();
-                        mSkills.setText(skills);
-                    }
-                    Glide.with(mProfileImage).clear(mProfileImage);
-                    if (map.get("profileImageUrl") != null) {
-                        profileImageUrl = map.get("profileImageUrl").toString().trim();
-                        switch (profileImageUrl) {
-                            case "default":
-                                Glide.with(getApplication()).load(R.mipmap.ic_launcher).into(mProfileImage);
-                                break;
-                            default:
-                                Glide.with(getApplication()).load(profileImageUrl).into(mProfileImage);
-                                break;
-                        }
-                    }
-                }
-            }
-
-            /**
-             *
-             * @param databaseError
-             */
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                return;
-            }
-        });
-    }
+//    /**
+//     *
+//     */
+//    private void getUserInfo() {
+//        mCustomerDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+//            /**
+//             *
+//             * @param dataSnapshot
+//             */
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+//                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+//
+//                    // Redundant, make method ******
+//                    if (map.get("name") != null) {
+//                        name = map.get("name").toString().trim();
+//                        mUserName.setText(name);
+//                    }
+//                    if (map.get("role") != null) {
+//                        String role = map.get("role").toString().trim();
+//                        mRole.setText(role);
+//                    }
+//                    if (map.get("skills") != null) {
+//                        String skills = map.get("skills").toString().trim();
+//                        mSkills.setText(skills);
+//                    }
+//                    Glide.with(mProfileImage).clear(mProfileImage);
+//                    if (map.get("profileImageUrl") != null) {
+//                        profileImageUrl = map.get("profileImageUrl").toString().trim();
+//                        switch (profileImageUrl) {
+//                            case "default":
+//                                Glide.with(getApplication()).load(R.mipmap.ic_launcher).into(mProfileImage);
+//                                break;
+//                            default:
+//                                Glide.with(getApplication()).load(profileImageUrl).into(mProfileImage);
+//                                break;
+//                        }
+//                    }
+//                }
+//            }
+//
+//            /**
+//             *
+//             * @param databaseError
+//             */
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                return;
+//            }
+//        });
+//    }
 
     /**
      *
